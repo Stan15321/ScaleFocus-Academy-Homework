f760aab9725bdb4e17e9226761b0c3786c38dcc5
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2023-02-23 19:55:04 +0200

    I removed the functions from the while loop

--- 01.CalculatorWithPythonHomework/Calculator with Python.py
+++ 01.CalculatorWithPythonHomework/Calculator with Python.py
@@ -1,3 +1,15 @@
+def funadd(a, b):
+    return a + b
+
+def funsubb(a,b):
+    return a-b
+
+def funmultiply(a, b):
+    return a * b
+
+def fundivide(a, b):
+    return a / b
+
 while True:
     operation = int(input("Select operation\n 1.Add \n 2.Subtract \n 3.Multiply\n 4.Devide \n Enter choice:(1/2/3/4): "))
     if operation == 1 or operation == 2 or operation == 3 or operation == 4:
@@ -12,25 +24,16 @@ while True:
         continue
 
     if operation == 1:
-        def funadd(a,b):
-            return a+b
         print(funadd(a,b))
     elif operation == 2:
-        def funsubb(a,b):
-            return a-b
         print(funsubb(a,b))
     elif operation == 3:
-        def funmultiply(a,b):
-            return a*b
         print(funmultiply(a,b))
     elif operation == 4:
         if b !=0:
-            def fundivide(a, b):
-                return a / b
             print(fundivide(a, b))
         else:
             print("You can't devide by 0")
-
 
     answer = input("Would you like to do another operation? yes/no: ")
     if answer == "no":
